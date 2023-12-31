12b1479dfaf408731910bed68c3b5e997f6750d1
AhmadSabbagh1 <109554944+AhmadSabbagh1@users.noreply.github.com>
2023-12-31 12:49:23 +0300

    Added the new edit for the home page

--- views/home.php
+++ views/home.php
@@ -12,17 +12,17 @@ require_once('../includes/partials/header.php')
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link text-white" aria-current="page" href="#">About</a>
+                <li class="nav-item my_nav">
+                <a class="nav-link text-white" aria-current="page" href="#About">About</a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link text-white" href="#">Our Services</a>
+                <li class="nav-item my_nav">
+                <a class="nav-link text-white" href="#OurServices">Our Services</a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link text-white" href="#">Workflow</a>
+                <li class="nav-item my_nav">
+                <a class="nav-link text-white" href="#Workflow">Workflow</a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link text-white">Contact Us</a>
+                <li class="nav-item my_nav">
+                <a class="nav-link text-white" href="#ContactUs">Contact Us</a>
                 </li>
             </ul>
             <div class="d-flex">
@@ -67,14 +67,14 @@ require_once('../includes/partials/header.php')
       </div>
     </div>
     <div class="carousel-item">
-      <img src="../assets/img/Second_slider2.jpg" class="d-block w-100 img-fluid" alt="...">
+      <img src="../assets/img/Second-slider.jpg" class="d-block w-100 img-fluid" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>COODE: Your Partner for Forward-Thinking Programming Solutions</h5>
         <p class="lead">COODE: Revolutionizing the programming landscape, we facilitate tailored connections between developers and clients, offering personalized consultations.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="../assets/img/Third_Slider2.jpeg" class="d-block w-100 img-fluid" alt="...">
+      <img src="../assets/img/keyboard.jpg" class="d-block w-100 img-fluid" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>COODE: Your Gateway to Top-Tier Programming Solutions</h5>
         <p class="lead">COODE offers an unparalleled experience for programmers and clients alike, serving as the gateway to premium programming solutions. Our comprehensive services feature expert consultations guiding clients to the right programmer.</p>
@@ -95,7 +95,7 @@ require_once('../includes/partials/header.php')
 
 
 <!--Start Home Page About-->
-<h3 class="text-center space_section"><strong>About</strong></h3>
+<h3 class="text-center space_section" id="About"><strong>About</strong></h3>
 <div class="About py-5 text-center">
   <div class="container">
     <div class="row align-items-center">
@@ -105,7 +105,7 @@ require_once('../includes/partials/header.php')
         <i class="fa-solid fa-calendar-days fa-5x my_IconeColor2"></i>
         <div class="card-body">
           <h4 class="card-title">Launch</h4>
-          <p class="card-text">Codee was launched in March 2024 by developers Mamdoh Zeyad and Ahmed Sabbagh</p>
+          <p class="card-text">Codee was launched in March 2024 by developers Mamdoh Zeyad and Ahmed Sabbagh.</p>
         </div>
       </div>
       </div>
@@ -115,7 +115,7 @@ require_once('../includes/partials/header.php')
         <i class="fa-solid fa-trophy fa-5x my_IconeColor"></i>
         <div class="card-body">
           <h4 class="card-title">Achievements</h4>
-          <p class="card-text">The website has been awarded first place for the Best Poster in Software Engineering Field in UOJ.</p>
+          <p class="card-text">The website has been awarded first place for the best poster in Software Engineering field in UOJ.</p>
         </div>
       </div>
       </div>
@@ -136,7 +136,7 @@ require_once('../includes/partials/header.php')
 
 
 <!-- Start Home Page Our Services -->
-<h3 class="text-center space_section "><strong>Our Services</strong></h3>
+<h3 class="text-center space_section" id="OurServices"><strong>Our Services</strong></h3>
 <div class="About py-5 text-center">
   <div class="container">
     <div class="row align-items-center">
@@ -156,7 +156,7 @@ require_once('../includes/partials/header.php')
         <img src="../assets/img/Consultant.png" class="card-img-top pt-4" alt="...">
         <div class="card-body">
           <h5 class="card-title">Consultant</h5>
-          <p class="card-text">consultants direct each coding issue to a skilled developer for resolution</p>
+          <p class="card-text">Consultants direct each coding issue to a skilled developer for resolution.</p>
         </div>
       </div>
     </div>
@@ -179,7 +179,7 @@ require_once('../includes/partials/header.php')
 <figure class="my_figure text-center p-4">
   <blockquote class="blockquote">
   <pre>Dedicated developers seriously refine the software to deliver an effortless 
-    experience for users.This sentence confirms the importance of attention 
+    experience for users. This sentence confirms the importance of attention 
     to detail and the commitment to quality that developers have.</pre>
   </blockquote>
   <div class="d-flex justify-content-center">
@@ -193,7 +193,7 @@ require_once('../includes/partials/header.php')
 
 
 <!-- Start Home Page Workflow-->
-<h3 class="text-center space_section"><strong>Work Flow</strong></h3>
+<h3 class="text-center space_section" id="Workflow"><strong>Work Flow</strong></h3>
 <div class="About pb-5 ">
   <div class="container">
   <div class="card-body" >
@@ -210,7 +210,7 @@ require_once('../includes/partials/header.php')
       <div class="card text-center">
             <i class="my_IconeColor fa-regular fa-address-card card-img-top pt-3" style="font-size: 5rem;"></i>
             <div class="card-body text-center">
-                <h5 class="card-title">The user will Register for his account</h5>
+                <h5 class="card-title">The User Will Register His Account</h5>
                 <p class="my_textCardColor card-text">Users promptly navigate to the login page with valid personal details.</p>
                 <input type="text" class="my_numberInsideCard" value="1" readonly>
             </div>
@@ -222,8 +222,8 @@ require_once('../includes/partials/header.php')
        <div class="card text-center">
             <i class="my_IconeColor2 fa-solid fa-user card-img-top pt-3" style="font-size: 5rem;"></i>
             <div class="card-body text-center">
-                <h5 class="card-title">The user will Log in to their account</h5>
-                <p class="my_textCardColor card-text">Upon entering valid credentials, permissions display based on user type</p>
+                <h5 class="card-title">The User Will Log In To Their Account</h5>
+                <p class="my_textCardColor card-text">Upon entering valid credentials, permissions display based on user type.</p>
                 <input type="text" class="my_numberInsideCard2" value="2" readonly>
             </div>
         </div>
@@ -233,8 +233,8 @@ require_once('../includes/partials/header.php')
     <div class="card text-center">
             <i class="my_IconeColor fa-solid fa-list card-img-top pt-3" style="font-size: 5rem;"></i>
             <div class="card-body text-center">
-                <h5 class="card-title">Browse developers & consultants</h5>
-                <p class="my_textCardColor card-text"> View available devs and consultants; choose based on specific needs</p>
+                <h5 class="card-title">Browse Developers & Consultants</h5>
+                <p class="my_textCardColor card-text"> View available Devs and consultants; choose based on specific needs.</p>
                 <input type="text" class="my_numberInsideCard" value="3" readonly>
             </div>
         </div>
@@ -245,7 +245,7 @@ require_once('../includes/partials/header.php')
       <div class="card text-center">
             <i class="my_IconeColor2 fa-solid fa-comments card-img-top pt-3" style="font-size: 5rem;"></i>
             <div class="card-body text-center">
-                <h5 class="card-title">Connect with specific devs or consultants</h5>
+                <h5 class="card-title">Connect with specific Devs or consultants</h5>
                 <p class="my_textCardColor card-text">The Users communicate to resolve issues or address unique needs</p>
                 <input type="text" class="my_numberInsideCard2" value="4" readonly>
               </div>
@@ -258,14 +258,14 @@ require_once('../includes/partials/header.php')
         
 
 <!-- Home Page Contact Us -->
-<h3 class="text-center space_section"><strong>Contact Us</strong></h3>
+<h3 class="text-center space_section" id="ContactUs"><strong>Contact Us</strong></h3>
 <div class="container-fluid About pb-4">
   <div class="row justify-content-center pt-4">
     <div class="col-md-4">
       <form class="needs-validation" novalidate>
         <div class="mb-3">
           <label for="validationCustom01" class="form-label">Full Name<span class="red_star">*</span></label>
-          <input type="text" class="form-control form-control-sm" id="validationCustom01" value="Mark" required>
+          <input type="text" class="form-control form-control-sm" id="validationCustom01" required>
           <div class="valid-feedback">Looks good!</div>
         </div>
 
